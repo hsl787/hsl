22f5edf3ec09c7986f425347a7a7392b149b9c3b
hsl <s4s3@naver.com>
2024-07-30 20:37:05 +0900

    Initial commit

---
+++ movie_kobis1.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+
+// KOBIS Open API를 호출하기 위한 클라이언트 서비스 정의
+class KobisOpenAPIRestService {
+    constructor(key, host) {
+        this.key = key;  // KOBIS API 키 (인증을 위한 키)
+        this.host = host || "http://www.kobis.or.kr";  // KOBIS API 호스트 URL, 기본값은 KOBIS 공식 호스트
+        // 다양한 KOBIS API 엔드포인트 URI 정의
+        this.DAILY_BOXOFFICE_URI = "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList";
+        this.WEEKLY_BOXOFFICE_URI = "/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList";
+        this.COM_CODE_LIST_URI = "/kobisopenapi/webservice/rest/code/searchCodeList";
+        this.MOVIE_LIST_URI = "/kobisopenapi/webservice/rest/movie/searchMovieList";
+        this.MOVIE_INFO_URI = "/kobisopenapi/webservice/rest/movie/searchMovieInfo";
+        this.COMPANY_LIST_URI = "/kobisopenapi/webservice/rest/company/searchCompanyList";
+        this.COMPANY_INFO_URI = "/kobisopenapi/webservice/rest/company/searchCompanyInfo";
+        this.PEOPLE_LIST_URI = "/kobisopenapi/webservice/rest/people/searchPeopleList";
+        this.PEOPLE_INFO_URI = "/kobisopenapi/webservice/rest/people/searchPeopleInfo";
+    }
+
+    // KOBIS API에 GET 요청을 보내는 함수 정의
+    async requestGet(serviceURI, paramMap) {
+        const urlStr = this.host + serviceURI + '.json';  // 요청할 URL 생성
+        paramMap.key = this.key;  // API 키를 파라미터에 추가
+
+        try {
+            const response = await axios.get(urlStr, { params: paramMap });
+            return response.data;  // 요청 성공 시 응답 데이터 반환
+        } catch (error) {
+            throw new Error(error.response.data);  // 오류 발생 시 에러 처리
+        }
+    }
+
+    // 일일 박스오피스 데이터 조회
+    async getDailyBoxOffice(paramMap) {
+        return await this.requestGet(this.DAILY_BOXOFFICE_URI, paramMap);
+    }
+}
+
+// KOBIS Open API 서비스 객체 생성
+const apiKey = "69897250c60e02be49d7b7dc362dc152";
+const apiHost = "http://www.kobis.or.kr";
+const kobisService = new KobisOpenAPIRestService(apiKey, apiHost);
+
+// 일일 박스오피스 데이터를 가져오기 위한 파라미터 설정
+const params = {
+    targetDt: "20230722"  // 조회할 날짜 (YYYYMMDD 형식)
+};
+
+// 일일 박스오피스 데이터를 JSON 형식으로 조회
+kobisService.getDailyBoxOffice(params)
+    .then(data => {
+        console.log(JSON.stringify(data, null, 2));  // JSON 형식으로 보기 좋게 출력
+    })
+    .catch(error => {
+        console.error("Error:", error.message);  // 오류 처리
+    });
---
+++ movie_kobis3.js
@@ -0,0 +1,86 @@
+
+const axios = require('axios');
+const { parse } = require('json2csv');
+const fs = require('fs');
+const { format, addDays, parseISO } = require('date-fns');
+
+// KOBIS Open API를 호출하기 위한 클라이언트 서비스 정의
+class KobisOpenAPIRestService {
+    constructor(key, host) {
+        this.key = key;
+        this.host = host || "http://www.kobis.or.kr";
+        this.DAILY_BOXOFFICE_URI = "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList";
+    }
+
+    async requestGet(serviceURI, paramMap) {
+        paramMap.key = this.key;
+        try {
+            const response = await axios.get(this.host + serviceURI + '.json', { params: paramMap });
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data);
+        }
+    }
+
+    async getDailyBoxOffice(paramMap) {
+        return await this.requestGet(this.DAILY_BOXOFFICE_URI, paramMap);
+    }
+}
+
+// 날짜 범위 설정
+const startDate = '20220720';
+const endDate = '20230722';
+
+// KOBIS Open API 서비스 객체 생성
+const apiKey = "69897250c60e02be49d7b7dc362dc152";
+const apiHost = "http://www.kobis.or.kr";
+const kobisService = new KobisOpenAPIRestService(apiKey, apiHost);
+
+// CSV 파일에 데이터를 추가하는 함수
+function appendDataToCSV(filePath, data) {
+    const csv = parse(data);
+    fs.appendFileSync(filePath, csv + '\n', 'utf8');
+}
+
+// 데이터 조회 및 저장
+async function fetchAndSaveBoxOfficeData(startDate, endDate) {
+    const filePath = 'dailyBoxOfficeData.csv';
+
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, 'date,rank,movieNm,audiAcc\n', 'utf8'); // 헤더 설정
+    }
+
+    let currentDate = parseISO(startDate);
+    const endDateObj = parseISO(endDate);
+    const totalDays = (endDateObj - currentDate) / (1000 * 60 * 60 * 24) + 1;
+    let processedCount = 0;
+
+    while (currentDate <= endDateObj) {
+        const currentDateString = format(currentDate, 'yyyyMMdd');
+        try {
+            const params = { targetDt: currentDateString };
+            const data = await kobisService.getDailyBoxOffice(params);
+            const boxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
+
+            if (boxOfficeList.length > 0) {
+                appendDataToCSV(filePath, boxOfficeList);
+                console.log(`Added data for ${currentDateString}.`);
+            } else {
+                console.log(`No data for ${currentDateString}.`);
+            }
+        } catch (error) {
+            console.error(`Error fetching data for ${currentDateString}:`, error.message);
+        }
+
+        // 진행 상황 출력
+        processedCount++;
+        console.log(`Progress: ${processedCount}/${totalDays} days processed.`);
+
+        // 다음 날짜로 이동
+        currentDate = addDays(currentDate, 1);
+    }
+}
+
+// 데이터 조회 및 저장
+fetchAndSaveBoxOfficeData(startDate, endDate);
+
